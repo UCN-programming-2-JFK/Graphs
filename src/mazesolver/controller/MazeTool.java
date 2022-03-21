7499ae54d82c5df05aeccd317bd18af59b87ce20
Jakob Farian Krarup <kr@rup.dk>
2022-03-21 23:38:45 +0100

    Maze generation implemented
    MazeTool fully implemented
    Full screen working
    Eternal loop mode (screensaver)

--- src/mazesolver/controller/MazeTool.java
+++ src/mazesolver/controller/MazeTool.java
@@ -10,63 +10,97 @@ public class MazeTool {
 
 	private final static Random random = new Random();
 
+	public static enum Direction{UP , RIGHT , DOWN , LEFT };
+	public static final Point UpDeltas = new Point(0,-1),RightDeltas = new Point(1,0), DownDeltas = new Point(0,1), LeftDeltas = new Point(-1,0); 
+	
 	private Maze maze;
-	private List<Point> tilesToCheck = new ArrayList<Point>(); 
+	private Stack<Point> tilesToCheck = new Stack<Point>(); 
 	public Maze getMaze() {return maze;}
 	public void setMaze(Maze maze) {this.maze = maze;}
-
+	private Point currentPoint;
+	public Point getCurrentPoint() {return currentPoint;}
+	
 	public MazeTool (int width, int height, Point startingPoint, Point endPoint) {
 		setMaze(new Maze(width, height, startingPoint, endPoint));
 		maze.fillAllTiles(true);
-		tilesToCheck.add(startingPoint);
+		currentPoint = startingPoint;
+		tilesToCheck.push(currentPoint);
 	}
 	
-	public void excavateNext() {
+	public void excavateNextd() {
 		//Point tileToCheck = tilesToCheck.remove(random.nextInt(tilesToCheck.size()));
-		Point tileToCheck = tilesToCheck.remove(tilesToCheck.size()-1);
+		//Point tileToCheck = tilesToCheck.remove(tilesToCheck.size()-1);
 		System.out.println("Testing " + tilesToCheck);
 		
-		excavateIfPossible(tilesToCheck, maze, tileToCheck);
+		//excavateIfPossible(tilesToCheck, maze, tileToCheck);
 	}
 	
-	private static void excavateIfPossible(List<Point> tilesToCheck, Maze maze, Point tileToCheck) {
-		if (maze.contains(tileToCheck) && isValidForExcavation(maze.getTiles(), tileToCheck)) {
-			maze.setTile(tileToCheck, false);
-			List<Point> neighbors = get4NeighborsNSEW(tileToCheck);
-			removeBorderTilesAndTilesOutsideMaze(maze.getTiles(), neighbors);
-			for (Point neighborTileToCheck : neighbors) {
-				if (isValidForExcavation(maze.getTiles(), neighborTileToCheck)) {
-					tilesToCheck.add(neighborTileToCheck);
-					System.out.println("Adding neighbor: " + neighborTileToCheck);
-				}
-			}
+	public void excavateNext() {		
+		
+		maze.setTile(currentPoint, false);
+		
+		List<Point> validNeighbors = getValidNeighbors(currentPoint);
+			
+		if(validNeighbors.size() > 0 && !currentPoint.equals(getMaze().getEndPoint())) {
+			tilesToCheck.push(currentPoint);
+			currentPoint = validNeighbors.get(random.nextInt(validNeighbors.size()));
 		}
+		else {
+			currentPoint = tilesToCheck.pop();
+		}
+		
+		
+//		if (isValidForExcavation(maze.getTiles(), tileToCheck)) {
+//			maze.setTile(tileToCheck, false);
+//			List<Point> deltas = getLeftRightAndForward(maze.getTiles(), tileToCheck);
+//			List<Point> neighbors = new ArrayList<Point>();
+//			for(Point delta : deltas) {
+//				neighbors.add(new Point(tileToCheck.x + delta.x, tileToCheck.y + delta.y));
+//			}
+//			
+//			for (Point neighborTileToCheck : neighbors) {
+//				if (isValidForExcavation(maze.getTiles(), neighborTileToCheck)) {
+//					tilesToCheck.add(neighborTileToCheck);
+//					System.out.println("Adding neighbor: " + neighborTileToCheck);
+//				}
+//			}
+//		}
 	}
 	
+	private List<Point> getValidNeighbors(Point tileToCheck) {
+		List<Point> neighbors = get4NeighborsNSEW(tileToCheck);
+		removeBorderTilesAndTilesOutsideMaze(maze.getTiles(), neighbors);
+		for(int neighBorIndex = neighbors.size()-1; neighBorIndex>= 0 ; neighBorIndex--) {
+			Point neighborToTest= neighbors.get(neighBorIndex);
+			if(!isValidForExcavation(neighborToTest))	 {neighbors.remove(neighBorIndex);}
+			
+		}
+		return neighbors;
+	}
 	public boolean isDone() {
 		return tilesToCheck.size() == 0;
 	}
 	
-	public static Maze createMaze(int width, int height, Point startingPoint, Point endingPoint) {
+//	public static Maze createMaze(int width, int height, Point startingPoint, Point endingPoint) {
+//
+//		Maze maze = new Maze(width, height);
+//		maze.fillAllTiles(true);
+//		List<Point> tilesToCheck = new ArrayList<Point>();
+//		tilesToCheck.add(startingPoint);
+//		do {
+//			//Point tileToCheck = tilesToCheck.remove(random.nextInt(tilesToCheck.size()));
+//			Point tileToCheck = tilesToCheck.remove(tilesToCheck.size()-1);
+//			System.out.println("Testing " + tilesToCheck);
+//			excavateIfPossible(tilesToCheck, maze, tileToCheck);
+//		} while (tilesToCheck.size() > 0);
+//
+//		return maze;
+//	}
 
-		Maze maze = new Maze(width, height);
-		maze.fillAllTiles(true);
-		List<Point> tilesToCheck = new ArrayList<Point>();
-		tilesToCheck.add(startingPoint);
-		do {
-			//Point tileToCheck = tilesToCheck.remove(random.nextInt(tilesToCheck.size()));
-			Point tileToCheck = tilesToCheck.remove(tilesToCheck.size()-1);
-			System.out.println("Testing " + tilesToCheck);
-			excavateIfPossible(tilesToCheck, maze, tileToCheck);
-		} while (tilesToCheck.size() > 0);
-
-		return maze;
-	}
-
-	private static boolean isValidForExcavation(boolean[][] tiles, Point tileToCheck) {
-		return  tiles[tileToCheck.x][tileToCheck.y] && numberOfFilled(tiles, get4NeighborsNSEW(tileToCheck)) >= 3
-				&& numberOfFilled(tiles, get8Neighbors(tileToCheck)) >= 6
-				&& !(tileToCheck.x % 2 ==0 && tileToCheck.y% 2 ==0) ;
+	private boolean isValidForExcavation( Point tileToCheck) {
+		return  maze.getTiles()[tileToCheck.x][tileToCheck.y] && numberOfFilled(maze.getTiles(), get4NeighborsNSEW(tileToCheck)) >= 3
+				&& !(tileToCheck.x % 2 ==0 && tileToCheck.y% 2 ==0) 
+				&& maze.getTiles()[tileToCheck.x][tileToCheck.y];
 	}
 
 	private static void removeBorderTilesAndTilesOutsideMaze(boolean[][] tiles, List<Point> neighbors) {
@@ -135,5 +169,23 @@ public class MazeTool {
 			}
 		}
 		return filled;
+	}
+	
+	public static List<Point> getLeftRightAndForward(boolean[][] tiles, Point currentTile){
+		if(!tiles[currentTile.x + UpDeltas.x][currentTile.y + UpDeltas.y]) {return getLeftRightAndForward(Direction.DOWN);}
+		else if(!tiles[currentTile.x + RightDeltas.x][currentTile.y + RightDeltas.y]) {return getLeftRightAndForward(Direction.LEFT);}
+		else if(!tiles[currentTile.x + DownDeltas.x][currentTile.y + DownDeltas.y]) {return getLeftRightAndForward(Direction.UP);}
+		else {return getLeftRightAndForward(Direction.RIGHT);}
+	}
+	
+	public static List<Point> getLeftRightAndForward(Direction direction){
+		if(direction == Direction.UP) {return new ArrayList<Point>(Arrays.asList(LeftDeltas, UpDeltas, RightDeltas));}
+		else if(direction == Direction.RIGHT) {return new ArrayList<Point>(Arrays.asList(UpDeltas, RightDeltas, DownDeltas));}
+		else if(direction == Direction.DOWN) {return new ArrayList<Point>(Arrays.asList(RightDeltas, DownDeltas, LeftDeltas));}
+		else {return new ArrayList<Point>(Arrays.asList(DownDeltas, LeftDeltas, UpDeltas));}
+	}
+	
+	public static List<Point> getAllDirectionDeltas(){
+		return new ArrayList<Point>(Arrays.asList(UpDeltas, RightDeltas, DownDeltas, LeftDeltas));
 	}
 }
--- src/mazesolver/gui/MazeFrame.java
+++ src/mazesolver/gui/MazeFrame.java
@@ -1,11 +1,11 @@
 package mazesolver.gui;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-import java.awt.Point;
+import java.awt.*;
+import java.awt.event.*;
 
 import javax.swing.*;
 
+import org.junit.jupiter.params.shadow.com.univocity.parsers.common.ColumnMap;
 import org.junit.validator.PublicClassValidator;
 
 import mazesolver.controller.MazeTool;
@@ -15,42 +15,75 @@ public class MazeFrame extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private static MazePanel panel;
 	private static boolean done = false;
+	private static int columns = 151, rows = 71, tileSizeInPixels = 12;
+
 	public static void main(String[] args) {
+
+		Dimension size = Toolkit. getDefaultToolkit(). getScreenSize();
+		columns = size.width / tileSizeInPixels;
+		rows = size.height/ tileSizeInPixels;
+		if(columns % 2 == 0) {columns --;}
+		if(rows% 2 == 0) {rows--;}
 		Runnable runner = new Runnable() {
 			public void run() {
-				
+
 				MazeFrame window = new MazeFrame();
 				window.setTitle("Maze visualizer");
-				window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+				window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); 
+				window.setUndecorated(true);
+				Rectangle r = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds();
+				window.setSize(r.width, r.height);
 				window.pack();
 				window.setVisible(true);
+				
+				KeyListener keyAdapter=new KeyAdapter()
+				 {
+				  public void keyPressed(KeyEvent evt)
+				  {
+					  //if(evt.getKeyCode()==KeyEvent.VK_ESCAPE)
+				   {
+				    System.exit(0);
+				   }
+				  }
+				 };
+				window.addKeyListener(keyAdapter);
 				done = true;
 			}
 		};
-			
+
 		EventQueue.invokeLater(runner);
-		while(!done) {
+		while (!done) {
 			System.out.println("not done");
-		};
-		MazeTool mazeTool = new MazeTool(17,  11,new Point(1,1) , new Point(15,9));
-		panel.setMaze(mazeTool.getMaze());
-		
-		while(!mazeTool.isDone()) {
+		}
+		;
+
+//		for (int mazeCounter = 0; mazeCounter < 200; mazeCounter++) 
+		while(true){
+			MazeTool mazeTool = new MazeTool(columns, rows, new Point(1, 1), new Point(columns - 2, rows - 2));
+			panel.setMaze(mazeTool.getMaze());
+
+			while (!mazeTool.isDone()) {
+				try {
+					Thread.sleep(1);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				mazeTool.excavateNext();
+				panel.repaint();
+			}
 			try {
-				Thread.sleep(100);
+				Thread.sleep(3000);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			panel.repaint();
-			mazeTool.excavateNext();
 		}
 	}
-	
-	public MazeFrame () {
+
+	public MazeFrame() {
 		getRootPane().setLayout(new BorderLayout());
-		panel = new MazePanel(17, 11, 50);
-		//panel.getMaze().addBorders();
+		panel = new MazePanel(columns, rows, tileSizeInPixels);
+		// panel.getMaze().addBorders();
 		JScrollPane scrollPane = new JScrollPane(panel);
 		getRootPane().add(scrollPane);
 	}
